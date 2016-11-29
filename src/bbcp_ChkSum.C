8e56659f25e8e5b178c78c0af756124550a401bb
Rodrigo Tobar <rtobar@icrar.org>
2016-11-29 10:52:29 +0800

    New zip-compatible CRC32 algorithm
    This CRC32 implementation is compatible with the ZIP formate CRC32
    implementation, and thus with a number of tools which us it instead of the
    POSIX CRC32 implementation (as implemented by bbcp_C32 and tools like cksum
    specified by the OpenGroup).
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/bbcp_ChkSum.C
+++ src/bbcp_ChkSum.C
@@ -31,6 +31,7 @@
 #include "bbcp_A32.h"
 #include "bbcp_A32_zlib.h"
 #include "bbcp_C32.h"
+#include "bbcp_C32_Zip.h"
 #include "bbcp_MD5.h"
 #include "bbcp_MD5_openssl.h"
 
@@ -50,6 +51,7 @@ bbcp_ChkSum *bbcp_ChkSum::Alloc(int csType)
 #else
           case bbcp_csMD5: return (bbcp_ChkSum *)new bbcp_MD5;
 #endif
+          case bbcp_csC32Z:return (bbcp_ChkSum *)new bbcp_C32_Zip;
           default:         break;
          }
 
